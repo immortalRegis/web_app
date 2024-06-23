0a1367c7f0d805040c8da47c603f127a8d2ba501
segun <segun.adeokun@gmail.com>
2024-06-22 18:38:29 -0600

    Add email form page features

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key="email_form"):
+    user_email = st.text_input('Enter your email address')
+    message = st.text_area('Your message')
+    button = st.form_submit_button()
